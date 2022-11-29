ddee07bd9844293337fed7d6dcdc24b85d58ac9d
zach <bluesky013@126.com>
2022-11-29 19:12:43 +0800

    native pipeline sample.

--- game/pipeline/NativePipelineSample.cpp
+++ game/pipeline/NativePipelineSample.cpp
@@ -1,9 +1,83 @@
 #include "pipeline/NativePipelineSample.h"
 
+// framework
+#include "application/ApplicationManager.h"
+#include "interfaces/modules/ISystemWindow.h"
+#include "interfaces/modules/ISystemWindowManager.h"
+
+// base
+#include "base/StringUtil.h"
+
+// render
+#include "scene/RenderWindow.h"
+
+// gfx
+#include "gfx-base/GFXDevice.h"
+
 namespace cc {
 
-void NativePipelineSample::onStart() {
+void NativePipelineSample::initScene() {
+    scene::IRenderSceneInfo sceneInfo = {"MainScene"};
+    _scene = ccnew scene::RenderScene();
+    _scene->initialize(sceneInfo);
 
+    _mainCamera = addCamera("MainCamera");
+    setActive(_mainCamera);
+}
+
+void NativePipelineSample::initRenderWindow(uint32_t windowId, ISystemWindow *window) {
+    auto handle = window->getWindowHandle();
+    const auto &size = window->getViewSize();
+
+    gfx::SwapchainInfo info;
+    info.width = static_cast<uint32_t>(size.width);
+    info.height = static_cast<uint32_t>(size.height);
+    info.windowHandle = reinterpret_cast<void *>(handle);
+    info.windowId = window->getWindowId();
+
+    gfx::Swapchain *swapChain = gfx::Device::getInstance()->createSwapchain(info);
+    _swapChains.emplace_back(swapChain);
+
+    scene::IRenderWindowInfo windowInfo = {};
+    windowInfo.title = StringUtil::format("renderWindow_%d", windowId);
+    windowInfo.width = swapChain->getWidth();
+    windowInfo.height = swapChain->getHeight();
+    windowInfo.swapchain = swapChain;
+
+    auto &passInfo = windowInfo.renderPassInfo;
+    gfx::ColorAttachment colorAttachment;
+    colorAttachment.format = swapChain->getColorTexture()->getFormat();
+    passInfo.colorAttachments.emplace_back(colorAttachment);
+
+    auto &depthStencilAttachment = passInfo.depthStencilAttachment;
+    depthStencilAttachment.format = swapChain->getDepthStencilTexture()->getFormat();
+    depthStencilAttachment.depthStoreOp = gfx::StoreOp::DISCARD;
+    depthStencilAttachment.stencilStoreOp = gfx::StoreOp::DISCARD;
+    _mainRenderWindow = ccnew scene::RenderWindow();
+    _mainRenderWindow->initialize(_device, windowInfo);
+
+    _mainCamera->changeTargetWindow(_mainRenderWindow);
+}
+
+void NativePipelineSample::onStart() {
+    _device = gfx::Device::getInstance();
+
+    auto *windowMgr = CC_GET_PLATFORM_INTERFACE(ISystemWindowManager);
+    const auto &windows = windowMgr->getWindows();
+    for (auto &[winId, sysWindow] : windows) {
+        initRenderWindow(winId, sysWindow.get());
+        break;
+    }
+
+    initScene();
+
+    _ppl.reset(render::Factory::createPipeline());
+    auto *layoutGraph = _ppl->getLayoutGraphBuilder();
+    uint32_t forwardColorId = layoutGraph->addRenderStage("forwardColor");
+    uint32_t forwardQueueId = layoutGraph->addRenderPhase("Queue", forwardColorId);
+
+    layoutGraph->compile();
+    layoutGraph->print();
 }
 
 void NativePipelineSample::onPause() {
@@ -19,6 +93,27 @@ void NativePipelineSample::onClose() {
 }
 
 void NativePipelineSample::onTick(float time) {
+    if (_ppl != nullptr && !_activeCameras.empty()) {
+        std::stable_sort(_activeCameras.begin(), _activeCameras.end(), [](const auto *a, const auto *b) {
+            return a->getPriority() < b->getPriority();
+        });
+        _ppl->render(_activeCameras);
+        _device->present();
+    }
+}
 
+
+scene::Camera *NativePipelineSample::addCamera(const ccstd::string &key) {
+    auto *camera = ccnew scene::Camera(_device);
+    _cameras.emplace(key, camera);
+    return camera;
+}
+
+void NativePipelineSample::setActive(scene::Camera *camera) {
+    _activeCameras.emplace_back(camera);
+}
+
+void NativePipelineSample::setDeActive(scene::Camera *camera) {
+    _activeCameras.erase(std::remove(_activeCameras.begin(), _activeCameras.end(), camera), _activeCameras.end());
 }
 }
--- game/pipeline/NativePipelineSample.h
+++ game/pipeline/NativePipelineSample.h
@@ -2,6 +2,14 @@
 
 #include "common/IGame.h"
 
+#include "base/std/container/unordered_map.h"
+#include "base/std/container/vector.h"
+#include "base/Ptr.h"
+#include "renderer/pipeline/custom/RenderInterfaceTypes.h"
+#include "scene/RenderWindow.h"
+#include "scene/RenderScene.h"
+#include "scene/Camera.h"
+
 namespace cc {
 
 class NativePipelineSample : public IGame {
@@ -15,6 +23,26 @@ private:
     void onResume() override;
     void onClose() override;
     void onTick(float time) override;
+
+    scene::Camera *addCamera(const ccstd::string &key);
+    void setActive(scene::Camera *camera);
+    void setDeActive(scene::Camera *camera);
+
+    void initScene();
+    void initRenderWindow(uint32_t windowId, ISystemWindow *window);
+
+    using CameraPtr = IntrusivePtr<scene::Camera>;
+
+    gfx::Device *_device = nullptr;
+    scene::Camera *_mainCamera = nullptr;
+    ccstd::vector<scene::Camera *> _activeCameras;
+    ccstd::unordered_map<ccstd::string, CameraPtr> _cameras;
+    std::unique_ptr<render::Pipeline> _ppl;
+    ccstd::vector<gfx::Swapchain *> _swapChains;
+    IntrusivePtr<scene::RenderScene> _scene;
+    IntrusivePtr<scene::RenderWindow> _mainRenderWindow;
+    events::WindowDestroy::Listener _windowDestroyListener;
+    events::WindowRecreated::Listener _windowRecreatedListener;
 };
 
 
